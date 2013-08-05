44c9393069c862accb8db1cb1f90ef7a694aba27
Istvan <lee.nover@gmail.com>
2013-08-05 16:20:04 +0200

    about view displays version info

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -1,18 +1,35 @@
 ﻿using Caliburn.Micro;
 using Bicikelj.Model;
 using System.Linq;
+using Bicikelj.Views;
+using BugSense;
 
 namespace Bicikelj.ViewModels
 {
     public class AboutViewModel : Screen
     {
         public string SupportedCities { get; set; }
+        public string AppTitle { get; set; }
+        public string VersionNumber { get; set; }
 
         protected override void OnActivate()
         {
             base.OnActivate();
             if (string.IsNullOrEmpty(SupportedCities))
                 UpdateCities();
+            if (string.IsNullOrEmpty(VersionNumber))
+                UpdateVersionInfo();
+        }
+
+        private void UpdateVersionInfo()
+        {
+            var info = new BugSense.Internal.ManifestAppInfo();
+            AppTitle = info.Title;
+            VersionNumber = info.Version;
+#if DEBUG
+            VersionNumber += " (debug)";
+#endif
+            NotifyOfPropertyChange(() => VersionNumber);
         }
 
         private void UpdateCities()
