7c942aa316c2a3be2f13862411bc87b460ba2007
Istvan <lee.nover@gmail.com>
2013-08-19 13:41:35 +0200

    fixed the samba service (varies across cities)

--- Model/BikeServiceProviders/SambaService.cs
+++ Model/BikeServiceProviders/SambaService.cs
@@ -18,7 +18,7 @@ namespace Bicikelj.Model
         protected override IList<City> GetCities()
         {
             var result = new List<City>() {
-                new City(){ CityName = "Pernambuco", Country = "Brasil", ServiceName = "BikePE", UrlCityName = "bikepe", Provider = Instance },
+                new City(){ CityName = "Recife", Country = "Brasil", ServiceName = "BikePE", UrlCityName = "bikepe", Provider = Instance },
                 new City(){ CityName = "Petrolina", Country = "Brasil", ServiceName = "PetroBike", UrlCityName = "petrobike", Provider = Instance },
                 new City(){ CityName = "Porto Alegre", Country = "Brasil", ServiceName = "bike PoA", UrlCityName = "bikepoa", Provider = Instance },
                 new City(){ CityName = "Porto Leve", Country = "Brasil", ServiceName = "PortoLeve", UrlCityName = "portoleve", Provider = Instance },
@@ -44,9 +44,22 @@ namespace Bicikelj.Model
         {
             var url = string.Format(StationListUrl, cityName);
             return DownloadUrl.GetAsync(url)
-                .Select<string, List<StationAndAvailability>>(s =>
+                .Select(s =>
                 {
-                    var sl = LoadStationsFromHTML(s, cityName);
+                    List<StationAndAvailability> sl;
+
+                    switch (cityName)
+                    {
+                        case "sambarjpt":
+                        case "bikesampa":
+                        case "sorocaba":
+                            sl = LoadStationsFromHTML_RIO(s, cityName);
+                            break;
+                        default:
+                            sl = LoadStationsFromHTML(s, cityName);
+                            break;
+                    }
+                    
                     UpdateAvailabilityCache(sl);
                     return sl;
                 });
@@ -65,7 +78,7 @@ namespace Bicikelj.Model
             while (dataPos > -1)
             {
                 dataPos += CDataStr.Length;
-                var dataEndPos = s.IndexOf(");", dataPos);
+                var dataEndPos = s.IndexOf(");", dataPos) - 1;
                 if (dataEndPos > -1 && s[dataEndPos] == '"')
                     dataEndPos--;
                 var dataStr = s.Substring(dataPos + 1, dataEndPos - dataPos);
@@ -95,44 +108,102 @@ namespace Bicikelj.Model
             return result;
         }
 
+        private static string Unescape(string s)
+        {
+            if (s == null || s.Length < 2 || s[0] != '\'' || s[s.Length - 1] != '\'')
+                return s;
+            else
+                return s.Substring(1, s.Length - 2);
+        }
+
+        private static string[] ParseFunctionParams(string s)
+        {
+            // parses 'quoted groups', doesn't handle nested quotes ' " '
+            var regex = new Regex(@"^(?:'(?<item>[^']*)'|(?<item>[^,]*))(?:,(?:'(?<item>[^']*)'|(?<item>[^,]*)))*$");
+            var array = regex
+              .Match(s)
+              .Groups["item"]
+              .Captures
+              .Cast<Capture>()
+              .Select(c => c.Value)
+              .ToArray();
+            return array;
+            /*
+            List<object> result = new List<object>();
+
+            int lastCopiedIdx = 0;
+            bool insideQuote = false;
+            char? currentQuoteChar = null;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (c == '\'' || c == '"')
+                {
+                    if (currentQuoteChar.HasValue && currentQuoteChar.Value == c)
+                    {
+                        insideQuote = false;
+                        result.Add(s.Substring(lastCopiedIdx + 1, i - lastCopiedIdx - 1));
+                        lastCopiedIdx = i;
+                    }
+                    else if (!currentQuoteChar.HasValue)
+                        currentQuoteChar = c;
+                }
+            }
+            
+            return result;*/
+        }
+
         private List<StationAndAvailability> LoadStationsFromHTML_RIO(string s, string cityName)
         {
             var result = new List<StationAndAvailability>();
-            const string CDataStr = "point = new GLatLng(";
+            const string CCoordStr = "point = new GLatLng(";
+            const string CDataStr = "criaPonto(point,";
 
-            int dataPos = s.IndexOf(CDataStr);
+            int dataPos = s.IndexOf(CCoordStr);
             if (dataPos > 0)
-                s = s.Substring(dataPos, s.IndexOf("function exibirEstacaMapa(", dataPos) - dataPos);
-            dataPos = s.IndexOf(CDataStr);
+                s = s.Substring(dataPos, s.IndexOf("function criaPonto(", dataPos) - dataPos);
+            dataPos = s.IndexOf(CCoordStr);
             string[] split = new string[] { "\",\"" };
             while (dataPos > -1)
             {
-                dataPos += CDataStr.Length;
+                dataPos += CCoordStr.Length;
+                // read the coordinate
                 var dataEndPos = s.IndexOf(");", dataPos);
-                if (dataEndPos > -1 && s[dataEndPos] == '"')
-                    dataEndPos--;
-                var dataStr = s.Substring(dataPos + 1, dataEndPos - dataPos);
-                var dataValues = Regex.Replace(dataStr, @"\t|\n|\r", "").Split(split, StringSplitOptions.None);
-                // stations without the 'address' are test stations
-                if (dataValues[9].Trim() != "")
+                var dataStr = s.Substring(dataPos, dataEndPos - dataPos);
+                var dataValues = dataStr.Split(',');
+
+                var station = new StationLocation();
+                station.Latitude = double.Parse(dataValues[0].Trim(), CultureInfo.InvariantCulture);
+                station.Longitude = double.Parse(dataValues[1].Trim(), CultureInfo.InvariantCulture);
+
+                dataPos = s.IndexOf(CDataStr, dataEndPos) + CDataStr.Length;
+                dataEndPos = s.IndexOf(");", dataPos);
+                dataStr = s.Substring(dataPos, dataEndPos - dataPos);
+                dataValues = ParseFunctionParams(dataStr);
+
+                int tmp = 0;
+                if (int.TryParse(dataValues[0], out tmp))
                 {
-                    var station = new StationLocation();
-                    station.Latitude = double.Parse(dataValues[0], CultureInfo.InvariantCulture);
-                    station.Longitude = double.Parse(dataValues[1], CultureInfo.InvariantCulture);
-                    station.Name = dataValues[3];
-                    station.Number = int.Parse(dataValues[4]);
-                    station.Address = dataValues[9];
+                    station.Number = tmp;
+                    station.Name = Unescape(dataValues[1]);
+                    station.Address = Unescape(dataValues[2]);
+                    station.FullAddress = Unescape(dataValues[3]);
                     station.City = cityName;
                     var availability = new StationAvailability();
-                    availability.Available = int.Parse(dataValues[7]);
-                    availability.Free = int.Parse(dataValues[8]);
-                    availability.Free -= availability.Available;
-                    availability.Connected = dataValues[7] == "A";
-                    availability.Open = dataValues[6] == "EO";
+
+                    if (int.TryParse(dataValues[8], out tmp))
+                        availability.Total = tmp;
+                    if (int.TryParse(dataValues[9], out tmp))
+                        availability.Available = tmp;
+                    if (availability.Total < availability.Available)
+                        availability.Total = availability.Available;
+                    availability.Free = availability.Total - availability.Available;
+                    availability.Connected = Unescape(dataValues[6]) == "A";
+                    availability.Open = Unescape(dataValues[7]) == "EO";
 
                     result.Add(new StationAndAvailability(station, availability));
                 }
-                dataPos = s.IndexOf(CDataStr, dataPos);
+                dataPos = s.IndexOf(CCoordStr, dataPos);
             }
 
             return result;
