54b342d6c21bd4419252cbc08632a3b0a559855a
Istvan Agoston <lee.nover@gmail.com>
2013-11-07 07:48:55 +0100

    renamed USA to United States

--- Model/BikeServiceProviders/BCycleService.cs
+++ Model/BikeServiceProviders/BCycleService.cs
@@ -21,23 +21,23 @@ namespace Bicikelj.Model
         protected override IList<City> GetCities()
         {
             var result = new List<City>() {
-                new City(){ CityName = "Battle Creek", Country = "USA", ServiceName = "B-cycle", UrlCityName = "battlecreek", Provider = Instance },
-                new City(){ CityName = "Boulder", Country = "USA", ServiceName = "B-cycle", UrlCityName = "boulder", Provider = Instance },
-                new City(){ CityName = "Broward", Country = "USA", ServiceName = "B-cycle", UrlCityName = "broward", Provider = Instance },
-                new City(){ CityName = "Charlotte", Country = "USA", ServiceName = "B-cycle", UrlCityName = "charlotte", Provider = Instance },
-                new City(){ CityName = "Denver", Country = "USA", ServiceName = "B-cycle", UrlCityName = "denver", Provider = Instance },
-                new City(){ CityName = "Des Moines", Country = "USA", ServiceName = "B-cycle", UrlCityName = "desmoines", Provider = Instance },
-                new City(){ CityName = "Fort Worth", Country = "USA", ServiceName = "B-cycle", UrlCityName = "fortworth", Provider = Instance },
-                new City(){ CityName = "Greenville", Country = "USA", ServiceName = "B-cycle", UrlCityName = "greenville", Provider = Instance },
-                new City(){ CityName = "Hawaii", Country = "USA", ServiceName = "B-cycle", UrlCityName = "hawaii", Provider = Instance },
-                new City(){ CityName = "Houston", Country = "USA", ServiceName = "B-cycle", UrlCityName = "houston", Provider = Instance },
-                new City(){ CityName = "Kansas City", Country = "USA", ServiceName = "B-cycle", UrlCityName = "kansascity", Provider = Instance },
-                new City(){ CityName = "Madison", Country = "USA", ServiceName = "B-cycle", UrlCityName = "madison", Provider = Instance },
-                new City(){ CityName = "Milwaukee", Country = "USA", ServiceName = "B-cycle", UrlCityName = "milwaukee", Provider = Instance },
-                new City(){ CityName = "Nashville", Country = "USA", ServiceName = "B-cycle", UrlCityName = "nashville", Provider = Instance },
-                new City(){ CityName = "Omaha", Country = "USA", ServiceName = "B-cycle", UrlCityName = "omaha", Provider = Instance },
-                new City(){ CityName = "San Antonio", Country = "USA", ServiceName = "B-cycle", UrlCityName = "sanantonio", Provider = Instance },
-                new City(){ CityName = "Spartanburg", Country = "USA", ServiceName = "B-cycle", UrlCityName = "spartanburg", Provider = Instance }
+                new City(){ CityName = "Battle Creek", Country = "United States", ServiceName = "B-cycle", UrlCityName = "battlecreek", Provider = Instance },
+                new City(){ CityName = "Boulder", Country = "United States", ServiceName = "B-cycle", UrlCityName = "boulder", Provider = Instance },
+                new City(){ CityName = "Broward", Country = "United States", ServiceName = "B-cycle", UrlCityName = "broward", Provider = Instance },
+                new City(){ CityName = "Charlotte", Country = "United States", ServiceName = "B-cycle", UrlCityName = "charlotte", Provider = Instance },
+                new City(){ CityName = "Denver", Country = "United States", ServiceName = "B-cycle", UrlCityName = "denver", Provider = Instance },
+                new City(){ CityName = "Des Moines", Country = "United States", ServiceName = "B-cycle", UrlCityName = "desmoines", Provider = Instance },
+                new City(){ CityName = "Fort Worth", Country = "United States", ServiceName = "B-cycle", UrlCityName = "fortworth", Provider = Instance },
+                new City(){ CityName = "Greenville", Country = "United States", ServiceName = "B-cycle", UrlCityName = "greenville", Provider = Instance },
+                new City(){ CityName = "Hawaii", Country = "United States", ServiceName = "B-cycle", UrlCityName = "hawaii", Provider = Instance },
+                new City(){ CityName = "Houston", Country = "United States", ServiceName = "B-cycle", UrlCityName = "houston", Provider = Instance },
+                new City(){ CityName = "Kansas City", Country = "United States", ServiceName = "B-cycle", UrlCityName = "kansascity", Provider = Instance },
+                new City(){ CityName = "Madison", Country = "United States", ServiceName = "B-cycle", UrlCityName = "madison", Provider = Instance },
+                new City(){ CityName = "Milwaukee", Country = "United States", ServiceName = "B-cycle", UrlCityName = "milwaukee", Provider = Instance },
+                new City(){ CityName = "Nashville", Country = "United States", ServiceName = "B-cycle", UrlCityName = "nashville", Provider = Instance },
+                new City(){ CityName = "Omaha", Country = "United States", ServiceName = "B-cycle", UrlCityName = "omaha", Provider = Instance },
+                new City(){ CityName = "San Antonio", Country = "United States", ServiceName = "B-cycle", UrlCityName = "sanantonio", Provider = Instance },
+                new City(){ CityName = "Spartanburg", Country = "United States", ServiceName = "B-cycle", UrlCityName = "spartanburg", Provider = Instance }
             };
             return result;
         }
