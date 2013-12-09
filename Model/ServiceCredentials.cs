4c2e69e70b0dda932baadbce73ad9fbf1df56310
Istvan <lee.nover@gmail.com>
2013-12-09 22:10:15 +0100

    added AdDeals/AdDuplex credentials

--- Model/ServiceCredentials.cs
+++ Model/ServiceCredentials.cs
@@ -4,17 +4,17 @@ namespace Bicikelj.Model
 {
     public static class BingMapsCredentials
     {
-        public static string Key = "your api key";
+        public static string Key = "your app key";
     }
 
     public static class BugSenseCredentials
     {
-        public static string Key = "your api key";
+        public static string Key = "your app key";
     }
 
     public static class AzureServiceCredentials
     {
-        public static string Key = "your api key";
+        public static string Key = "your app key";
         public static Dictionary<string, string> AuthHeaders;
 
         static AzureServiceCredentials()
@@ -26,6 +26,17 @@ namespace Bicikelj.Model
 
     public static class FlurryCredentials
     {
-        public static string Key = "your api key";
+        public static string Key = "your app key";
+    }
+
+    public static class AdDealsCredentials
+    {
+        public static string ID = "id";
+        public static string Key = "your app key";
+    }
+
+    public static class AdDuplexCredentials
+    {
+        public static string ID = "your app key";
     }
 }
