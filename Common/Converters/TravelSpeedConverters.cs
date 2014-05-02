728cb7a08195ef7c43914aa4d330ceb6f4a8f359
Istvan <lee.nover@gmail.com>
2014-05-02 18:45:29 +0200

    * TravelSpeedConverter checks the parameter for max value and uses relative conversion

--- Common/Converters/TravelSpeedConverters.cs
+++ Common/Converters/TravelSpeedConverters.cs
@@ -5,53 +5,62 @@ using System.Windows;
 
 namespace Bicikelj.Converters
 {
-	public class TravelSpeedConverter : IValueConverter
-	{
+    public class TravelSpeedConverter : IValueConverter
+    {
 
-		#region IValueConverter Members
+        #region IValueConverter Members
 
-		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			object result = DependencyProperty.UnsetValue;
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            object result = DependencyProperty.UnsetValue;
 
-			if ((targetType == typeof(int) || targetType == typeof(double)) && value is TravelSpeed)
-				result = TravelSpeedToInt((TravelSpeed)value);
-			else if (targetType == typeof(string))
-				result = TravelSpeedToString((TravelSpeed)value);
+            if ((targetType == typeof(int) || targetType == typeof(double)) && value is TravelSpeed)
+                result = TravelSpeedToInt((TravelSpeed)value, parameter);
+            else if (targetType == typeof(string))
+                result = TravelSpeedToString((TravelSpeed)value);
 
-			return result;
-		}
+            return result;
+        }
 
-		public static int TravelSpeedToInt(TravelSpeed speed)
-		{
-			return speed == TravelSpeed.Slow ? 0 : speed == TravelSpeed.Normal ? 1 : 2;
-		}
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            object result = DependencyProperty.UnsetValue;
 
-		public static TravelSpeed TravelSpeedFromDouble(double speed)
-		{
-			if (speed <= 0.66)
-				return TravelSpeed.Slow;
-			else if (speed >= 1.33)
-				return TravelSpeed.Fast;
-			else
-				return TravelSpeed.Normal;
-		}
+            if (targetType == typeof(TravelSpeed) && (value is int) || (value is double))
+                result = TravelSpeedFromDouble((double)value, parameter);
 
-		public static string TravelSpeedToString(TravelSpeed speed)
-		{
-			return speed.ToString().ToLower();
-		}
+            return result;
+        }
 
-		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			object result = DependencyProperty.UnsetValue;
+        public static int TravelSpeedToInt(TravelSpeed speed, object parameter)
+        {
+            int max = 2;
+            if (parameter is double || parameter is int)
+                max = (int)parameter;
+            int mid = (int)Math.Round(max / 2d);
+            return speed == TravelSpeed.Slow ? 0 : speed == TravelSpeed.Normal ? mid : max;
+        }
 
-			if (targetType == typeof(TravelSpeed) && (value is int) || (value is double))
-				result = TravelSpeedFromDouble((double)value);
-			
-			return result;
-		}
+        public static TravelSpeed TravelSpeedFromDouble(double speed, object parameter)
+        {
+            double max = 2;
+            if (parameter is double || parameter is int)
+                max = (double)parameter;
+            speed = speed / max;
 
-		#endregion
-	}
+            if (speed <= 0.33)
+                return TravelSpeed.Slow;
+            else if (speed >= 0.66)
+                return TravelSpeed.Fast;
+            else
+                return TravelSpeed.Normal;
+        }
+
+        public static string TravelSpeedToString(TravelSpeed speed)
+        {
+            return speed.ToString().ToLower();
+        }
+
+        #endregion
+    }
 }
