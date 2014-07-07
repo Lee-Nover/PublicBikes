e74632e514a989920a2f0571a2212628f078ae36
Istvan <lee.nover@gmail.com>
2014-07-07 17:01:17 +0200

    updated city coordinated and radii

--- Common/Model/BikeServiceProvider.cs
+++ Common/Model/BikeServiceProvider.cs
@@ -108,8 +108,8 @@ namespace Bicikelj.Model
                     if (cities != null && cities.Count > 0)
                         allCities.AddRange(cities);
                 }
-                /*ExportCityCoordinates();*/
                 UpdateCityCoordinates();
+                //ExportCityCoordinates();
             }
             return allCities;
         }
@@ -158,7 +158,7 @@ namespace Bicikelj.Model
             foreach (var city in GetAllCities())
             {
                 //if (city.Latitude != 0 && city.Longitude != 0)
-                if (city.Radius > 0)
+                if (city.Radius > 1)
                     continue;
 
                 List<StationLocation> _stations = null;
@@ -193,6 +193,7 @@ namespace Bicikelj.Model
                         + stationsArea.Center.GetDistanceTo(stationsArea.Northwest)
                         + stationsArea.Center.GetDistanceTo(stationsArea.Southeast)
                         + stationsArea.Center.GetDistanceTo(stationsArea.Southwest)) / 4;
+                    city.Radius = Math.Max(city.Radius, 1000);
                 }
             }
             SaveCityCoordinates(allCities);
